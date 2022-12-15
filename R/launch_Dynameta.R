948e3e2937fde3897c86a4103ba06ba9cfbad571
Grace Skinner <GraSki@ceh.ac.uk>
2022-12-15 12:56:30 +0000

    adding sample_data documentation

--- R/launch_Dynameta.R
+++ R/launch_Dynameta.R
@@ -8,8 +8,11 @@
 #' @export
 launch_Dynameta <- function() {
   
-  # Get app directory 
+  # Get app directory - don't include "inst" here as doesn't exist when you install the package from github
   appDir <- base::system.file("Dynameta_app", package = "Dynameta")
+  
+  # To test app before pushing to git:
+  #runApp("inst/Dynameta_app/", display.mode = "normal")
   
   # Run app
   shiny::runApp(appDir, display.mode = "normal")
