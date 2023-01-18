8a3cd18dce892012b5c3f16c35393046521b265b
Grace Skinner <GraSki@ceh.ac.uk>
2023-01-18 16:55:17 +0000

    shinytest2 try 2

--- inst/Dynameta_app/tests/testthat/test-shinytest2.R
+++ inst/Dynameta_app/tests/testthat/test-shinytest2.R
@@ -3,5 +3,6 @@ library(shinytest2)
 test_that("{shinytest2} recording: shinytest2_test", {
   app <- AppDriver$new(name = "shinytest2_test", height = 1076, width = 1832)
   app$set_inputs(map_center = c(27.421875, 28.3043806829628), allow_no_input_binding_ = TRUE)
-  app$expect_values()
+  app$set_inputs(threat = "9 Pollution")
+  app$expect_values(output = "threat_details_table")
 })
